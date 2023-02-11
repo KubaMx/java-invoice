e90b776bded9e94fc58027014ce3a229b2b8f27c
Jakub Makowiecki <makowiecki@student.agh.edu.pl>
2023-02-11 12:06:33 +0100

    all invoice tests working

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -11,22 +11,41 @@ public class Invoice {
     private final Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
+        if(product == null){
+            throw new IllegalArgumentException("product is null");
+        }
         this.products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity){
+        if(product == null){
+            throw new IllegalArgumentException("product is null");
+        }
+        if(quantity <= 0){
+            throw new IllegalArgumentException("quantity cannot be less than 1");
+        }
         this.products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        return products
+                .entrySet()
+                .stream()
+                .map(e -> e.getKey().getPrice().multiply(new BigDecimal(e.getValue())))
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
     }
 
     public BigDecimal getTax() {
-        return null;
+       return getTotal().subtract(getSubtotal());
     }
 
     public BigDecimal getTotal() {
-        return null;
+        return products
+                .entrySet()
+                .stream()
+                .map(e -> e.getKey().getPriceWithTax().multiply(new BigDecimal(e.getValue())))
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
     }
 }
